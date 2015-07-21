ae1f53e6a9c46d48fd37c494915d3006140aa5d7
codechaotic <thinkinfractals@gmail.com>
2015-07-20 23:06:14 -0400

    Create simple test

---
+++ tasks/test.js
@@ -0,0 +1,28 @@
+(function() {
+  "use strict";
+
+  var gulp = require('gulp'),
+      Q = require('q')
+
+  gulp.task('test', test)
+
+  function test() {
+    ping('http://localhost')
+      .then( function(up) {
+        if(up) console.log('RUNNING')
+        else console.log('CAN\'T REACH')
+      })
+  }
+
+  function ping(url) {
+    var deferred = Q.defer()
+    http.get(url, function(res) {
+      if(res.statusMessage === 'OK' ) deferred.resolve(true)
+    }).on('error', function(e) {
+      if(e.code === 'ECONNREFUSED') deferred.resolve(false)
+      deferred.reject(new Error('ping failed'))
+    })
+    return deferred.promise
+  }
+
+})();
