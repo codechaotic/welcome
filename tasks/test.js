528d187970b05a61cf874a856121c724ee5a9e31
codechaotic <thinkinfractals@gmail.com>
2015-07-20 23:31:40 -0400

    Detangent

--- tasks/test.js
+++ tasks/test.js
@@ -8,29 +8,7 @@
   gulp.task('test', test)
 
   function test(done) {
-    ping('http://localhost')
-      .then( function(up) {
-        if(up) console.log('RUNNING')
-        else console.log('CAN\'T REACH')
-      })
-      .then(done)
-  }
-
-  function ping(url) {
-    var deferred = Q.defer()
-    var req = http.get(url, function(res) {
-      if(res.statusMessage === 'OK' ) deferred.resolve(true)
-    }).on('error', function(e) {
-      if(e.code === 'ECONNREFUSED') deferred.resolve(false)
-      deferred.reject(new Error('ping failed'))
-    }).on('socket', function (socket) {
-      socket.setTimeout(10000)
-      socket.on('timeout', function() {
-        req.abort()
-      })
-    })
-
-    return deferred.promise
+    console.log('tests not yet implemented')
   }
 
 })();
