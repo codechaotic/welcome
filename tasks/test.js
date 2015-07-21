ff40e12f84c4de80e095178005b8d8a17b0db0b3
codechaotic <thinkinfractals@gmail.com>
2015-07-20 23:19:34 -0400

    Add timeout to test

--- tasks/test.js
+++ tasks/test.js
@@ -7,22 +7,29 @@
 
   gulp.task('test', test)
 
-  function test() {
+  function test(done) {
     ping('http://localhost')
       .then( function(up) {
         if(up) console.log('RUNNING')
         else console.log('CAN\'T REACH')
       })
+      .then(done)
   }
 
   function ping(url) {
     var deferred = Q.defer()
-    http.get(url, function(res) {
+    var req = http.get(url, function(res) {
       if(res.statusMessage === 'OK' ) deferred.resolve(true)
     }).on('error', function(e) {
       if(e.code === 'ECONNREFUSED') deferred.resolve(false)
       deferred.reject(new Error('ping failed'))
+    }).on('socket', function (socket) {
+      socket.setTimeout(10000)
+      socket.on('timeout', function() {
+        req.abort()
+      })
     })
+
     return deferred.promise
   }
 
