3a2296523647920f39c4821a0c857873982b76e3
codechaotic <thinkinfractals@gmail.com>
2015-08-02 01:12:52 -0400

    fix protractor test?

--- protractor.config.js
+++ protractor.config.js
@@ -1,9 +1,13 @@
+var test_port = process.env.PORT || 8080
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  baseUrl: 'http://localhost',
+  baseUrl: 'http://localhost:' + test_port,
   specs: ['test/spec.js'],
   capabilities: {
     'browserName': 'phantomjs',
     'phantomjs.binary.path': require('phantomjs').path,
   }
 }
+
+console.log(JSON.stringify(exports.config,null,'  '))
--- src/server/config.js
+++ src/server/config.js
@@ -3,7 +3,7 @@
   //@autoexport
   module.exports.config = {
     mongo_url:          process.env.MONGO_URL,
-    port:               process.env.PORT,
+    port:               process.env.PORT || 8080,
     public_dir:         'public'
   }
 
