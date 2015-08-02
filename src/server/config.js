c13e43c2425c2e068b5b866a8a74d04fc535b194
codechaotic <thinkinfractals@gmail.com>
2015-08-02 00:48:47 -0400

    change test port to 8080

--- src/server/config.js
+++ src/server/config.js
@@ -3,7 +3,7 @@
   //@autoexport
   module.exports.config = {
     mongo_url:          process.env.MONGO_URL,
-    port:               process.env.PORT | 8080,
+    port:               process.env.PORT,
     public_dir:         'public'
   }
 
